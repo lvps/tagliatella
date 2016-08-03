8f39b857b1ea8f48142c1f14969b5cc1b736e2ec
Ludovico Pavesi <lvps@users.noreply.github.com>
2016-08-03 17:39:47 +0200

    Handle timezones

--- config.php
+++ config.php
@@ -5,3 +5,5 @@ define('DB_NAME', 'tagliatella'); // database name
 define('DB_HOST', 'mysql.local');
 define('DB_PORT', '3306');
 define('CONFERENCE_ID', 1);
+define('DB_TIMEZONE', 'UTC');
+define('OUTPUT_TIMEZONE', 'Europe/Rome');
