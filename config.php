90e06280faf25fd8babbcf58ebf82436cbad4a30
Ludovico Pavesi <lvps@users.noreply.github.com>
2016-08-03 19:14:19 +0200

    Changed default timezone behavior

--- config.php
+++ config.php
@@ -6,4 +6,4 @@ define('DB_HOST', 'mysql.local');
 define('DB_PORT', '3306');
 define('CONFERENCE_ID', 1);
 define('DB_TIMEZONE', 'UTC');
-define('OUTPUT_TIMEZONE', 'Europe/Rome');
+define('OUTPUT_TIMEZONE', 'UTC');
