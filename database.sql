f35365f2c472ea3e22ac5b4186805f0cd2303089
Ludovico Pavesi <lvps@users.noreply.github.com>
2016-08-05 21:42:08 +0200

    Removed CREATE DATABASE

--- database.sql
+++ database.sql
@@ -3,9 +3,6 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-CREATE DATABASE `tagliatella` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
-USE `tagliatella`;
-
 CREATE TABLE `conferences` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL,
